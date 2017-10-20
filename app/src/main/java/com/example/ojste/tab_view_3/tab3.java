92c672fd9d0216fa997fa837ab99a3795734caa2
LuxJahi <ojstewart@Hotmail.com>
2017-10-19 22:28:55 -0700

    Initial commit

---
+++ app/src/main/java/com/example/ojste/tab_view_3/tab3.java
@@ -0,0 +1,21 @@
+package com.example.ojste.tab_view_3;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ojste on 10/16/2017.
+ */
+
+public class tab3 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_3_frag,container,false);
+    }
+}
